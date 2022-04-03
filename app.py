67c43b9e912d24f38c82fb5145f38f2b85be8513
Wei Ching Lim <weiching.lim06@gmail.com>
2022-04-03 14:08:57 -0500

    Test for successful application run

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pageTitle='Flask Server Home Page')
 
 @app.route('/mike')
 def mike():
-    return render_template('mike.html')
+    return render_template('mike.html', pageTitle='About Mike')
 
 if __name__ == '__main__':
     app.run(debug=True)
