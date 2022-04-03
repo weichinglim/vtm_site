a46e3e33c5a08bd887235ef809fbf04f994243d5
Wei Ching Lim <weiching.lim06@gmail.com>
2022-04-03 16:28:40 -0500

    Added and linked all 3 pages for vtm_site

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle='Flask Server Home Page')
+    return render_template('index.html', pageTitle='Vertical Tank Maintenance')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html', pageTitle='About Mike')
+@app.route('/about')
+def about():
+    return render_template('about.html', pageTitle='Vertical Tank Maintenance')
+
+@app.route('/estimate')
+def estimate():
+    return render_template('estimate.html', pageTitle='Tank Painting Estimate')
 
 if __name__ == '__main__':
     app.run(debug=True)
