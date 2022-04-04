cfef57bfc17148c333aa95ddebc2a1f041064713
Wei Ching Lim <weiching.lim06@gmail.com>
2022-04-03 19:40:08 -0500

    Created form and included calculations

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -11,9 +11,41 @@ def index():
 def about():
     return render_template('about.html', pageTitle='Vertical Tank Maintenance')
 
-@app.route('/estimate')
+@app.route('/estimate', methods=['POST', 'GET'])
 def estimate():
-    return render_template('estimate.html', pageTitle='Tank Painting Estimate')
+    price=''
+    if request.method == 'POST':
+        form = request.form
+        tankHeight = float(form['height'])
+        tankRadius = float(form['radius'])
+
+        #Area of tank top
+        pi = 3.14
+        area_top = 2*(pi*tankRadius*tankRadius)
+
+        #Area of tank sides
+        area_sides = 2*(pi*(tankRadius*tankHeight))
+
+        #Calculate total area
+        total_area = area_top+area_sides
+
+        #Convert total area in inches to square feet
+        total_sf = total_area/144
+
+        #Calculate material cost
+        materialCost = 25.00 #per square foot
+        total_mc = total_sf*materialCost
+
+        #Calculate labor cost
+        laborCost = 15.00 #per square foot
+        total_lc = total_sf*laborCost
+
+        #Total price
+        total_ce = total_mc+total_lc #total_ce = total cost estimate
+
+        #return redirect(url_for('index'))
+        return render_template('estimate.html', price=total_ce)
+    return render_template('estimate.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
